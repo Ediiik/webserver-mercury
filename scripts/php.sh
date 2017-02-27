a427153c6baa76e2c599d0c6c6be173e562fd163
Tomas Gustinak <tgustinak@trionyx.sk>
2017-02-27 10:51:04 +0100

    Added new php modules

--- scripts/php.sh
+++ scripts/php.sh
@@ -10,4 +10,16 @@ sudo apt-get -y install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql
 echo ""
 echo ""
 echo "## -> Installing additional PHP packages"
-sudo apt-get -y install php7.0-curl php7.0-json php7.0-cgi
+sudo apt-get -y install php7.0-curl php7.0-json php7.0-cgi php7.0-zip
+
+# enable mysqlnd module
+sudo phpenmod mysqlnd
+
+# enable pdo module
+sudo phpenmod pdo
+
+# restart apache
+echo ""
+echo ""
+echo "## -> Restarting Apache"
+sudo systemctl restart apache2
