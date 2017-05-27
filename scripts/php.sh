4b1ecbce7c0b348ea452e70f2779d0e0314d03ba
Tomáš Gustiňák - TRIONYX <tgustinak@trionyx.sk>
2017-05-27 20:34:36 +0200

    Added PHP MBstring extension

--- scripts/php.sh
+++ scripts/php.sh
@@ -10,7 +10,16 @@ sudo apt-get -y install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql
 echo ""
 echo ""
 echo "## -> Installing additional PHP packages"
-sudo apt-get -y install php7.0-curl php7.0-json php7.0-cgi php7.0-zip php-gd imagick php-imagick php7.0-intl
+sudo apt-get -y install php7.0-curl
+sudo apt-get -y install php7.0-json
+sudo apt-get -y install php7.0-cgi
+sudo apt-get -y install php7.0-zip
+sudo apt-get -y install php-gd
+sudo apt-get -y install imagick
+sudo apt-get -y install php-imagick
+sudo apt-get -y install php7.0-intl
+sudo apt-get -y install php7.0-mbstring
+
 
 # enable mysqlnd module
 sudo phpenmod mysqlnd
